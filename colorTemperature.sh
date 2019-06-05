b56801995417b092c8cb4f8470024e013081e845
Turiano <romero.guzman18@gmail.com>
2019-06-05 16:07:01 -0500

    script para calibrar color en pantalla

---
+++ colorTemperature.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#Instalación de redshift
+#->: sudo apt install redshift
+#------------------------------------------------------
+
+# script para bajar la temperatura del color  linux 
+redshift -l 55.7:12.6 -t 5700:3200 -g 0.8 -m randr -v
+
+#------------------------------------------------------
